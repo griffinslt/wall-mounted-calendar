624269d3e0f5915a983d34978cf4f45b21ea23e6
griffinslt <slt.griffin3105@gmail.com>
2023-01-28 14:52:16 +0000

    Relationship between models set up

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+
+    public function bookings()
+    {
+        return $this->hasMany(User::class);
+    }
 }
