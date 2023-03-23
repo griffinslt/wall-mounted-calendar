2d1150dd71f4b5e793908a6a3f3c8181adc9985f
griffinslt <slt.griffin3105@gmail.com>
2023-03-23 16:11:44 +0000

    Redundant code removed in TabletController

--- app/Http/Controllers/TabletController.php
+++ app/Http/Controllers/TabletController.php
@@ -18,17 +18,12 @@ class TabletController extends Controller
         $rooms = Room::all();
         $buildings = Building::all();
 
-        
+
         $room_id = $request->cookie("tablet_room");
-        // echo $value;
 
         if ($room_id) {
             $room = Room::find($room_id);
             return view('tablet-view', ['bookings' => $bookings, 'rooms' => $rooms, 'room' => $room, 'buildings' => $buildings]);
-        // } else if(auth()->check()){
-        //     if (auth()->user()->can('view-all-tablets')) {
-        //         return view('tablet-view', ['bookings' => $bookings, 'rooms' => $rooms, 'room' => $room, 'buildings' => $buildings]);
-        //     }
         }
         abort(403);
     }
@@ -43,10 +38,7 @@ class TabletController extends Controller
 
 
         auth()->logout();
-        // if (has already been made) {
-        //     just assign it that cookie
-        // }
-        $room = $validatedData['room'];
+        
         $minutes = 5259492;
         $value = $validatedData['room'];
         $response = new Response(redirect()->route('tablet-view'));
@@ -81,31 +73,31 @@ class TabletController extends Controller
         $room_id = $request->cookie("tablet_room");
         if ($room_id) {
             $room = Room::find($room_id);
-        
 
-        $executed = RateLimiter::attempt(
-            $room_id,
-            $perMinute = 5,
-            function () {
 
+            $executed = RateLimiter::attempt(
+                $room_id,
+                5,
+                function () {
+
+                }
+            );
+
+            if (!$executed) {
+                session()->flash('error', 'Too Many Issues Reported, Try Again In A Minute');
+                return redirect()->route('tablet-view');
             }
-        );
 
-        if (!$executed) {
-            session()->flash('error', 'Too Many Issues Reported, Try Again In A Minute');
+            Mail::raw("Tablet from room " . $room->room_number . " on level " . $room->level . "in building " . $room->building->name . " on " . $room->building->campus . " Campus is have an issue with " . $issue, function ($message) {
+                $message->from('tablet-issue@university.com', 'Laravel');
+
+                $message->to('support@univeristy.com');
+            });
+
+            session()->flash('message', 'Issue Reported');
             return redirect()->route('tablet-view');
         }
-
-        Mail::raw("Tablet from room " . $room->room_number . " on level " . $room->level . "in building " . $room->building->name . " on " . $room->building->campus . " Campus is have an issue with " . $issue, function ($message) {
-            $message->from('tablet-issue@university.com', 'Laravel');
-
-            $message->to('support@univeristy.com');
-        });
-        //return view('make_booking', ['bookings' => $bookings, 'rooms' => $rooms, 'room' => $room, 'buildings' => $buildings]);
-        session()->flash('message', 'Issue Reported');
-        return redirect()->route('tablet-view');
+        abort(403);
     }
-    abort(403);
-}
 
 }
