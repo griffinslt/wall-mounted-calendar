1a0f8f011026b488a2f7dbc81954a94c58065cda
griffinslt <slt.griffin3105@gmail.com>
2023-04-05 10:49:02 +0100

    auth check added to user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,56 +8,21 @@ use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $users = User::paginate(30);
+        if (!auth()->check()) {
+            return view('auth.login');
+        }
 
-        return view('admin.users.users', ['users' => $users]);
+        if (auth()->user()->can('view-all-users')) {
+            $users = User::paginate(30);
+
+            return view('admin.users.users', ['users' => $users]);
+        }
+
+        abort(403);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit(User $user)
     {
         $roles = Role::all();
@@ -71,13 +36,7 @@ class UserController extends Controller
         return view('admin.users.edit', ['user' => $user, 'roles' => $roles_to_show]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(Request $request, User $user)
     {
 
@@ -90,9 +49,9 @@ class UserController extends Controller
         $user->name = $validatedData['name'];
         $user->save();
 
-        
 
-        return redirect()->route('users.index')->with('message','User ('.$user->name.') updated');
+
+        return redirect()->route('users.index')->with('message', 'User (' . $user->name . ') updated');
     }
 
     public function removeRoleFromUser(User $user, Role $role)
@@ -107,20 +66,12 @@ class UserController extends Controller
         return redirect()->route('users.edit', ['user' => $user->id])->with('message', 'Role was added');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(User $user)
     {
-
-
         $user->delete();
         return redirect()
-                ->route('users.index')
-                ->with('message', 'User was Deleted.');
-        
+            ->route('users.index')
+            ->with('message', 'User was Deleted.');
+
     }
 }
